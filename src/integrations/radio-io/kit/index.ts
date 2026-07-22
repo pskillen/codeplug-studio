4bccbda3541de1c40ead2a945cac3b9f58b8157d
Patrick Skillen <patrick.skillen2@nhs.scot>
2026-07-22 21:05:34 +0100

    feat(radio-io): shared types and typed radio errors

---
+++ src/integrations/radio-io/kit/index.ts
@@ -0,0 +1,11 @@
+export {
+  RadioAbortedError,
+  RadioClosedError,
+  RadioIoError,
+  type RadioIoErrorCode,
+  RadioNoResponseError,
+  RadioProtocolError,
+  RadioTimeoutError,
+  RadioUnsupportedError,
+  RadioWrongIdentError,
+} from './errors.ts';
